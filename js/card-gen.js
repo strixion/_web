c293111a6ff96d267a4c5c735df07b373535f8fe
DENSENSE <167194090+DENSENSE@users.noreply.github.com>
2024-10-26 17:34:39 +0300

    Обновляемся

--- js/card-gen.js
+++ js/card-gen.js
@@ -2,87 +2,97 @@ document.body.onload = MembersApp();
 document.body.onload = scrr();
 
 window.onscroll = scrr();
-setTimeout(function(){
-	document.body.classList.add('body_visible');
+setTimeout(function () {
+  document.body.classList.add("body_visible");
 }, 200);
 
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+document.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && (e.key === "c" || e.key === "x")) {
+    e.preventDefault();
+  }
+});
 
 function MembersApp() {
   //список челов= [ИМЯ ФАМИЛИЯ      ДОЛЖНОСТЬ          АДРЕС-КАРТИНКИ],
-    var members = [
-      ['Денис Астапенко', 'Фронтенд', "./images/member3.jpg"],
-      ['Родион Бушуев', 'Тим-лид', "./images/member1.png"],
-      ['Ксения Попкова', 'UX-UI дизайнер', "./images/member2.png"],
-      ['Дмитрий Елькин', 'Маркетолог', "./images/member5.jpeg"],
-      ['Руслан Маннанов', 'Бэкэнд', "./images/member4.png"]
-                  ];
-    
-    var howmany_members = members.length;
-    document.getElementById('people').style.width = (howmany_members * 300) + "px";
-    console.log(document.getElementById('people').style.width);
-    // пробегаем по каждому челу и делаем карточки на странице
-    members.forEach((element) => addMember.apply(null, element));
+  var members = [
+    ["Родион Бушуев", "Teamlead, Fullstack", "./images/member1.png"],
+    ["Денис Астапенко", "Frontend", "./images/member3.jpg"],
+    ["Руслан Маннанов", "Backend", "./images/member4.png"],
+    ["Ксения Попкова", "UX-UI designer", "./images/member2.png"],
+    ["Дмитрий Елькин", "Marketer", "./images/member5.jpeg"],
+  ];
+
+  var howmany_members = members.length;
+  document.getElementById("people").style.width = howmany_members * 300 + "px";
+  console.log(document.getElementById("people").style.width);
+  // пробегаем по каждому челу и делаем карточки на странице
+  members.forEach((element) => addMember.apply(null, element));
 }
-function addMember(nme, imgg, rolee) 
-{
-    var div = document.createElement('div');
-    div.setAttribute('class', 'team-member');
-    div.setAttribute('data-name', nme);
-    div.setAttribute('data-img', imgg);
-    div.setAttribute('data-role', rolee);
-    div.innerHTML = `
+function addMember(nme, imgg, rolee) {
+  var div = document.createElement("div");
+  div.setAttribute("class", "team-member");
+  div.setAttribute("data-name", nme);
+  div.setAttribute("data-img", imgg);
+  div.setAttribute("data-role", rolee);
+  div.innerHTML = `
     <div class="card">
         <img src=${rolee} alt=${nme}/>
         <h3>${nme}</h3>
     </div>
     `;
-    console.log(imgg);
-    document.getElementById('people').appendChild(div);
-
+  console.log(imgg);
+  document.getElementById("people").appendChild(div);
 }
 
-const modal = document.getElementById('modal');
-const closeModal = document.querySelector('.close');
-const modalImage = document.getElementById('modal-image');
-const modalName = document.getElementById('modal-name');
-const modalRole = document.getElementById('modal-role');
+const modal = document.getElementById("modal");
+const closeModal = document.querySelector(".close");
+const modalImage = document.getElementById("modal-image");
+const modalName = document.getElementById("modal-name");
+const modalRole = document.getElementById("modal-role");
 
 function openModal(imageSrc, name, role) {
   modalImage.src = imageSrc;
   modalName.textContent = name;
   modalRole.textContent = role;
-  modal.style.display = 'flex';
+  modal.style.display = "flex";
 }
 function closeModalFunction() {
-    modal.style.display = 'none';
+  modal.style.display = "none";
+}
+
+closeModal.addEventListener("click", closeModalFunction);
+window.addEventListener("click", function (event) {
+  if (event.target === modal) {
+    closeModalFunction();
   }
-  
-  closeModal.addEventListener('click', closeModalFunction);
-window.addEventListener('click', function(event) {
-    if (event.target === modal) {
-      closeModalFunction();
-    }
-  });
+});
 
-document.querySelectorAll('.team-member').forEach(member => {
-    member.addEventListener('click', () => {
-      const imageSrc = member.getAttribute('data-role');
-      const name = member.getAttribute('data-name');
-      const role = member.getAttribute('data-img');
-      openModal(imageSrc, name, role);
-    });
+document.querySelectorAll(".team-member").forEach((member) => {
+  member.addEventListener("click", () => {
+    const imageSrc = member.getAttribute("data-role");
+    const name = member.getAttribute("data-name");
+    const role = member.getAttribute("data-img");
+    openModal(imageSrc, name, role);
   });
+});
 
-  var bsDiv = document.getElementById("box-shadow-div");
-  var x, y;
-  window.addEventListener('mousemove', function(event){
+var bsDiv = document.getElementById("box-shadow-div");
+var x, y;
+window.addEventListener(
+  "mousemove",
+  function (event) {
     x = event.clientX;
     y = event.clientY;
-    if ( typeof x !== 'undefined' ){
+    if (typeof x !== "undefined") {
       bsDiv.style.left = x + "px";
       bsDiv.style.top = y + "px";
     }
-  }, false);
+  },
+  false
+);
 function scrr() {
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   console.log(scrollTop);
--- js/script.js
+++ js/script.js
@@ -1,148 +1,133 @@
 document.body.onload = randcol();
 document.body.onload = ktrue();
+window.onscroll = scrr();
 
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+document.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && (e.key === "c" || e.key === "x")) {
+    e.preventDefault();
+  }
+});
 
+var bsDiv = document.getElementById("box-shadow-div");
+var x, y;
+window.addEventListener(
+  "mousemove",
+  function (event) {
+    x = event.clientX;
+    y = event.clientY;
+    if (typeof x !== "undefined") {
+      bsDiv.style.left = x + "px";
+      bsDiv.style.top = y + "px";
+    }
+  },
+  false
+);
 function randcol() {
-  var_colors = ['#424242', '#626262', '525252', '#323232', '#727272', '#828282', '#474747'];
-  card1 = document.getElementById('card1').style.backgroundColor = var_colors[(Math.floor(Math.random() * var_colors.length))];
-  card2 = document.getElementById('card2').style.backgroundColor = var_colors[(Math.floor(Math.random() * var_colors.length))];
-  card3 = document.getElementById('card3').style.backgroundColor = var_colors[(Math.floor(Math.random() * var_colors.length))];
-  card4 = document.getElementById('card4').style.backgroundColor = var_colors[(Math.floor(Math.random() * var_colors.length))];
-  card5 = document.getElementById('card5').style.backgroundColor = var_colors[(Math.floor(Math.random() * var_colors.length))];
+  var_colors = [
+    "#424242",
+    "#626262",
+    "525252",
+    "#323232",
+    "#727272",
+    "#828282",
+    "#474747",
+  ];
+  card1 = document.getElementById("card1").style.backgroundColor =
+    var_colors[Math.floor(Math.random() * var_colors.length)];
+  card2 = document.getElementById("card2").style.backgroundColor =
+    var_colors[Math.floor(Math.random() * var_colors.length)];
+  card3 = document.getElementById("card3").style.backgroundColor =
+    var_colors[Math.floor(Math.random() * var_colors.length)];
+  card4 = document.getElementById("card4").style.backgroundColor =
+    var_colors[Math.floor(Math.random() * var_colors.length)];
+  card5 = document.getElementById("card5").style.backgroundColor =
+    var_colors[Math.floor(Math.random() * var_colors.length)];
 }
 
-
-function ktrue () {
-k = true;
+function ktrue() {
+  k = true;
 }
 function previewchart1(k) {
-  console.log(k)
+  console.log(k);
   thiscanvas = document.getElementById("canvas1");
   if (k == true) {
-    thiscanvas.style = "transform: scale(2);"
+    thiscanvas.style = "transform: scale(2);";
     k == false;
   } else {
-    thiscanvas.style = "transform: scale(1);"
-    k == true
+    thiscanvas.style = "transform: scale(1);";
+    k == true;
   }
 }
-window.addEventListener('scroll', () => {
-  const features = document.querySelectorAll('.feature');
+window.addEventListener("scroll", () => {
+  const features = document.querySelectorAll(".feature");
   const screenPosition = window.innerHeight / 1.3;
 
-  features.forEach(feature => {
+  features.forEach((feature) => {
     const featurePosition = feature.getBoundingClientRect().top;
     if (featurePosition < screenPosition) {
-      feature.classList.add('active');
+      feature.classList.add("active");
     }
   });
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    const targetId = this.getAttribute('href');
+    const targetId = this.getAttribute("href");
     document.querySelector(targetId).scrollIntoView({
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   });
 });
 
-const modal = document.getElementById('modal');
-const closeModal = document.querySelector('.close');
-const modalImage = document.getElementById('modal-image');
-const modalName = document.getElementById('modal-name');
-const modalRole = document.getElementById('modal-role');
+const modal = document.getElementById("modal");
+const closeModal = document.querySelector(".close");
+const modalImage = document.getElementById("modal-image");
+const modalName = document.getElementById("modal-name");
+const modalRole = document.getElementById("modal-role");
 
 function openModal(imageSrc, name, role) {
   modalImage.src = imageSrc;
   modalName.textContent = name;
   modalRole.textContent = role;
-  modal.style.display = 'flex';
+  modal.style.display = "flex";
 }
 
 function closeModalFunction() {
-  modal.style.display = 'none';
+  modal.style.display = "none";
 }
 
-closeModal.addEventListener('click', closeModalFunction);
+closeModal.addEventListener("click", closeModalFunction);
 
-window.addEventListener('click', function(event) {
+window.addEventListener("click", function (event) {
   if (event.target === modal) {
     closeModalFunction();
   }
 });
 
-document.querySelectorAll('.team-member').forEach(member => {
-  member.addEventListener('click', () => {
-    const imageSrc = member.getAttribute('data-img');
-    const name = member.getAttribute('data-name');
-    const role = member.getAttribute('data-role');
+document.querySelectorAll(".team-member").forEach((member) => {
+  member.addEventListener("click", () => {
+    const imageSrc = member.getAttribute("data-img");
+    const name = member.getAttribute("data-name");
+    const role = member.getAttribute("data-role");
     openModal(imageSrc, name, role);
   });
 });
 
-
-const ctxLine = document.getElementById('lineChart').getContext('2d');
-const lineChart = new Chart(ctxLine, {
-  type: 'line',
-  data: {
-    labels: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июн'],
-    datasets: [{
-      label: 'Рост продаж',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-const ctxPie = document.getElementById('pieChart').getContext('2d');
-const pieChart = new Chart(ctxPie, {
-  type: 'pie',
-  data: {
-    labels: ['Рекомендации блюд', 'Оптимизация запасов', 'Поддержка персонала'],
-    datasets: [{
-      label: 'Процент использования',
-      data: [55, 25, 20],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(75, 192, 192, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(75, 192, 192, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: true,
-  }
-});
-
-
-const stats = document.querySelectorAll('.stat-number');
+const stats = document.querySelectorAll(".stat-number");
 const speed = 200; // Скорость анимации
 
-stats.forEach(stat => {
+stats.forEach((stat) => {
   const updateCount = () => {
-    const target = +stat.getAttribute('data-target');
+    const target = +stat.getAttribute("data-target");
     const count = +stat.innerText;
 
     const increment = target / speed;
 
-    if(count < target) {
+    if (count < target) {
       stat.innerText = Math.ceil(count + increment);
       setTimeout(updateCount, 10);
     } else {
@@ -158,7 +143,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("scroll", () => {
     const currentScrollY = window.pageYOffset;
-    
+
     if (lastScrollY > currentScrollY) {
       // Скролл вверх
       header.classList.add("shrink");
@@ -173,18 +158,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 var item = document.querySelector("#bullet-cursor");
 
-function You_are_here() 
-{
-  here = document.getElementById('here-man');
+function You_are_here() {
+  here = document.getElementById("here-man");
   here.innerText = "Уже здесь!";
 }
 
-block.onmousemove = function(e) {
-item.style.position = 'fixed';
-item.style.left = e.clientX + -20 + 'px';
-item.style.top = e.clientY + -20 + 'px';
-}
+block.onmousemove = function (e) {
+  item.style.position = "fixed";
+  item.style.left = e.clientX + -20 + "px";
+  item.style.top = e.clientY + -20 + "px";
+};
 
-function go_home() {
-  
+function scrr() {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  console.log(scrollTop);
 }
